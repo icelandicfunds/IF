3e192de797bbf9018a4e2e4a4a33dc76de0ad078
s-pace <sylvain.pace@algolia.com>
2019-11-19 11:51:35 +0100

    wip: use ErrorBoundary to catch error

--- src/components/DocSearch/index.js
+++ src/components/DocSearch/index.js
@@ -1,23 +1,37 @@
 import React, { useRef, useEffect } from 'react';
+import ErrorBoundary from '../ErrorBoundary';
 
 export default function DocSearch({ indexName, apiKey }) {
-  console.log(apiKey);
-
   const docsearchRef = useRef(null);
   useEffect(() => {
-    if (!docsearchRef.current) {
+    if (!docsearchRef.current || !docsearchRef.current.id) {
       return;
     }
-    console.log(docsearchRef);
+    console.log(docsearchRef.current.id);
+    console.log(docsearchRef.current);
+    if (docsearchRef.current.class) {
+      console.log(docsearchRef.current.class)
+    }
 
-    import('docsearch.js').then(({ default: docsearch }) => {
-      docsearch({
-        apiKey,
-        indexName,
-        inputSelector: `#${docsearchRef.current.id}`,
-        debug: false, // Set debug to true if you want to inspect the dropdown
+    try {
+      import('docsearch.js').then(({ default: docsearch }) => {
+        docsearchRef.current.id &&
+          docsearch({
+            apiKey,
+            indexName,
+            inputSelector: `#${docsearchRef.current.id}`,
+            debug: false, // Set debug to true if you want to inspect the dropdown
+          });
       });
-    });
-  }, [docsearchRef]);
-  return <input id="q" ref={docsearchRef} />;
+    } catch (error) {
+      console.error('catched');
+      console.error(error);
+      // Note - error messages will vary depending on browser
+    }
+  }, [docsearchRef, indexName, apiKey]);
+  return (
+    <ErrorBoundary>
+      <input id="q" ref={docsearchRef} />
+    </ErrorBoundary>
+  );
 }
